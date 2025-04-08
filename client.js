6f8890ef7b1fd2e716c46c3e9d7b4c646013f592
yordan-gergov01 <yordansoftskills@gmail.com>
2025-04-08 13:27:21 +0300

    managing backpressure

--- client.js
+++ client.js
@@ -1,2 +1,25 @@
 const net = require("net");
 const fs = require("node:fs/promises");
+
+const socket = net.createConnection({ host: "::1", port: 5050 }, async () => {
+  const filePath = "./text.txt";
+  const fileHandle = await fs.open(filePath, "r");
+  const fileReadStream = fileHandle.createReadStream();
+
+  // Reading from the source file
+  fileReadStream.on("data", (data) => {
+    // handling back-pressure
+    if (!socket.write(data)) {
+      fileReadStream.pause();
+    }
+  });
+
+  socket.on("drain", () => {
+    fileReadStream.resume();
+  });
+
+  filReadeStream.on("end", () => {
+    console.log("The file was successfully uploaded!");
+    socket.end();
+  });
+});
--- server.js
+++ server.js
@@ -3,15 +3,42 @@ const fs = require("node:fs/promises");
 
 const server = net.createServer(() => {});
 
+let fileHandle, fileWriteStream;
+
 // this socket refers to the client endpoint
 // and it's actually a duplex stream
 server.on("connection", (socket) => {
   console.log("New connection!");
 
   socket.on("data", async (data) => {
-    const fileHandle = await fs.open(`storage/test.txt`, "w");
-    const fileStream = fileHandle.createWriteStream();
-    fileStream.write(data);
+    if (!fileHandle) {
+      socket.pause(); // pause receiving data from the client
+      fileHandle = await fs.open(`storage/test.txt`, "w");
+      fileWriteStream = fileHandle.createWriteStream();
+
+      // Writing to our destination file
+      fileWriteStream.write(data);
+
+      socket.resume(); //resume receiving data from the client
+
+      fileWriteStream.on("drain", () => {
+        socket.resume();
+      });
+    } else {
+      if (!fileWriteStream.write(data)) {
+        console.log("Buffer is full, pausing...");
+
+        socket.pause();
+      }
+    }
+  });
+
+  socket.on("end", () => {
+    fileHandle.close();
+    fileHandle = undefined;
+    fileWriteStream = undefined;
+    socket.end();
+    console.log("Connection ended!");
   });
 });
 
